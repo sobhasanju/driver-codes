56c2da9b0d17174a89d5b49265a4870af7d7d16b
sobhasanju <103244157+sobhasanju@users.noreply.github.com>
2022-04-08 12:51:53 +0530

    Update java-driver-code.java

--- numbers/java-driver-code.java
+++ numbers/java-driver-code.java
@@ -1,28 +1,29 @@
-import java.io.*;
-import java.util.*;
+  import java.io.BufferedReader;
+  import java.io.InputStreamReader;
+  import java.math.BigInteger;
+  import java.util.ArrayList;
+  import java.util.List;
 
-public class Main {
-  	// The main function reads the input in the required format.
-  	// Just complete the solve function below.
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter wr = new PrintWriter(System.out);
-        int T = Integer.parseInt(br.readLine().trim());
-        for(int t_i = 0; t_i < T; t_i++)
-        {
-            int n =  Integer.parseInt(br.readLine());
-            String out_ = solve(n);
-            System.out.println(out_);
-         }
-         wr.close();
-         br.close();
-    }
-  
-    static String solve(int n){
-      	// Your code goes here
-        // n is the given input
-      
-        String result = "Special";
-        return result;
-    }
-}
+  class TestClass {
+      public static void main(String args[]) throws Exception {
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          String line = br.readLine();
+          int N = Integer.parseInt(line);
+          ArrayList<Integer> inputs = new ArrayList<Integer>();
+          for (int i = 0; i < N; i++) {
+              inputs.add(Integer.valueOf(br.readLine()));
+          }
+          ArrayList<BigInteger> outputs = new ArrayList<BigInteger>();
+          for(Integer input : inputs){
+              outputs.add(factorial(input));
+          }
+          for(BigInteger result: outputs){
+              System.out.println(result);
+          }
+      }
+
+      private static BigInteger factorial(Integer input) {
+          if(input == 1) return BigInteger.ONE;
+          return factorial(input - 1).multiply(new BigInteger(String.valueOf(input)));
+      }
+  }
